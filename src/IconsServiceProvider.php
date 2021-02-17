348b80915ea889324fddb5c98a3fd2f3a62767c6
Leibbrandt <n.leibbrandt@pure-mind-solutions.de>
2021-02-17 15:01:11 +0100

    IconsServiceProvider Refactoring

--- src/IconsServiceProvider.php
+++ src/IconsServiceProvider.php
@@ -13,36 +13,10 @@ class IconsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'icons');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'icons');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/tall-icons.php' => config_path('tall-icons.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/icons'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/icons'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/icons'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
         }
     }
 
@@ -55,11 +29,6 @@ class IconsServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/../config/tall-icons.php', 'tall-icons');
 
         $this->registerComponents();
-
-        // Register the main class to use with the facade
-//        $this->app->singleton('icons', function () {
-//            return new Icons;
-//        });
     }
 
     /**
